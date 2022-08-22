0195784f098c17add1030d0f66429af7efd0e2c8
Jawad Tahir <ranajawadtahir@gmail.com>
2022-08-22 19:48:03 +0200

    Tidying up

--- operations-playground/3_experiment_start.sh
+++ operations-playground/3_experiment_start.sh
@@ -1,33 +1,11 @@
 #!/usr/bin/env bash
 
-docker_swarm_volume_prune()
-{
-  NODE_NAME=$1
-  echo "SSH into ${NODE_NAME}"
-  ssh -o StrictHostKeyChecking=no ubuntu@$NODE_NAME docker volume prune -f
-}
-
-NODE_LIST=("node1")
-
-#for i in "${NODE_LIST[@]}"; do
-#  docker_swarm_volume_prune "$i"
-#done
 
 # Remove any previous utils containers
 echo "Removing any previous utils containers..."
 docker stack rm utils
 
-#echo "Sleeping for 15 seconds"
-#sleep 15s
-#
-## Remove any previous volumes
-#echo "Removing any orphan volumes..."
-##docker volume prune -f
-#for i in "${NODE_LIST[@]}"; do
-#  docker_swarm_volume_prune "$i"
-#done
-
-export DELAY_COUNT=5
+export DELAY_COUNT=15
 
 # Start utils containers
 echo "Starting utils containers..."
