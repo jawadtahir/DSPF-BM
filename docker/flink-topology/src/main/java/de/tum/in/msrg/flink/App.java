2264e03020ba9769d87be268b6cbf5bbc5ff984a
Jawad Tahir <ranajawadtahir@gmail.com>
2023-09-14 10:09:16 +0200

    Major commit. Correct results. Updated Flink to 1.17

--- docker/flink-topology/src/main/java/de/tum/in/msrg/flink/App.java
+++ docker/flink-topology/src/main/java/de/tum/in/msrg/flink/App.java
@@ -13,6 +13,7 @@ import org.apache.flink.configuration.*;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -22,6 +23,7 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.OutputTag;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -46,51 +48,31 @@ public class App
         LOGGER.info(String.format("kafka: %s", kafka));
 //        String pg = cmdLine.getOptionValue("pg", "exactly once");
 
-        Configuration configuration = new Configuration();
-//        configuration.set(TaskManagerOptions.NUM_TASK_SLOTS, 2);
-//        configuration.set(CoreOptions.DEFAULT_PARALLELISM, 2);
-//        configuration.set(RestOptions.PORT, 48081);
-
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
-
-
-        Properties srcProperties = new Properties();
-
-        srcProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka);
-        srcProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        srcProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "flink-topology");
 
 
         Properties sinkProperties = new Properties();
 
         sinkProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka);
-        sinkProperties.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 3000);
+        sinkProperties.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 30000);
 
 
 
 
-        CustomKafkaSource<ClickEvent> clickSource = CustomKafkaSource.<ClickEvent>builder()
+        KafkaSource<ClickEvent> clickSource = KafkaSource.<ClickEvent>builder()
                 .setTopics("click")
                 .setBootstrapServers(kafka)
-                .setStartingOffsets(OffsetsInitializer.earliest())
-                .setProperties(srcProperties)
-                .setProperty(KafkaSourceOptions.CLIENT_ID_PREFIX.key(), "flink-topology")
+                .setGroupId("clickReader")
+                .setStartingOffsets(OffsetsInitializer.committedOffsets(OffsetResetStrategy.EARLIEST))
                 .setValueOnlyDeserializer(new ClickEventDeserializer())
                 .build();
 
-//        CustomKafkaSource<UpdateEvent> updateSource = CustomKafkaSource.<UpdateEvent>builder()
-//                .setTopics("update")
-//                .setBootstrapServers(kafka)
-//                .setProperties(srcProperties)
-//                .setProperty(KafkaSourceOptions.CLIENT_ID_PREFIX.key(), "flink-topology")
-//                .setValueOnlyDeserializer(new UpdateEventDeserializer())
-//                .build();
 
         KafkaSink<PageStatistics> statsSink = KafkaSink.<PageStatistics>builder()
                 .setBootstrapServers(kafka)
                 .setKafkaProducerConfig(sinkProperties)
-                .setDeliverGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
-                .setTransactionalIdPrefix("stat-sink").
+                .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                .setTransactionalIdPrefix("statSink").
                 setRecordSerializer(KafkaRecordSerializationSchema.builder()
                         .setTopic("output")
                         .setValueSerializationSchema(new PageStatisticsValueSerializer())
@@ -101,8 +83,8 @@ public class App
         KafkaSink<ClickUpdateEvent> lateSink = KafkaSink.<ClickUpdateEvent>builder()
                 .setBootstrapServers(kafka)
                 .setKafkaProducerConfig(sinkProperties)
-                .setDeliverGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
-                .setTransactionalIdPrefix("late-sink").
+                .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                .setTransactionalIdPrefix("lateSink").
                 setRecordSerializer(KafkaRecordSerializationSchema.builder()
                         .setTopic("lateOutput")
                         .setValueSerializationSchema(new ClickUpdateValueSerializer())
@@ -113,33 +95,17 @@ public class App
 
 
         WatermarkStrategy<ClickEvent> clickEventWatermarkStrategy = WatermarkStrategy
-                .<ClickEvent>forBoundedOutOfOrderness(Duration.ofMillis(200))
-                .withIdleness(Duration.ofMillis(1000))
+                .<ClickEvent>forMonotonousTimestamps()
                 .withTimestampAssigner((element, recordTimestamp) -> element.getTimestamp().getTime());
-
-//        WatermarkStrategy<UpdateEvent> updateEventWatermarkStrategy = WatermarkStrategy
-//                .<UpdateEvent>forBoundedOutOfOrderness(Duration.ofMillis(200))
-//                .withIdleness(Duration.ofMillis(200))
-//                .withTimestampAssigner((element, recordTimestamp) -> element.getTimestamp().getTime());
-
 
 
         DataStreamSource<ClickEvent> clickSrc = environment.fromSource(clickSource, clickEventWatermarkStrategy, "ClickEvent-src");
-//        DataStreamSource<UpdateEvent> updateSrc = environment.fromSource(updateSource, updateEventWatermarkStrategy, "UpdateEvent-src");
-
-//        DataStream<ClickUpdateEvent> joinedStream = clickSrc.join(updateSrc)
-//                .where(ClickEvent::getPage).equalTo(UpdateEvent::getPage)
-//                .window(TumblingEventTimeWindows.of(Time.seconds(60)))
-//                .allowedLateness(Time.seconds(20))
-//                .apply(new ClickUpdateJoinFunction());
 
         OutputTag<ClickUpdateEvent> outputTag = new OutputTag<ClickUpdateEvent>("lateEvent", TypeInformation.of(ClickUpdateEvent.class));
-//        OutputTag<PageStatistics> outputTag1 = new OutputTag<PageStatistics>("lateEvent", TypeInformation.of(PageStatistics.class));
 
         SingleOutputStreamOperator<PageStatistics> statsStream = clickSrc.map(value -> new ClickUpdateEvent(value, null))
                 .keyBy(ClickUpdateEvent::getPage)
                 .window(TumblingEventTimeWindows.of(Time.seconds(60)))
-//                .allowedLateness(Time.seconds(20))
                 .sideOutputLateData(outputTag)
                 .process(new CountProcessWindowFunction());
 
--- docker/flink-topology/src/main/java/de/tum/in/msrg/flink/App1.java
+++ docker/flink-topology/src/main/java/de/tum/in/msrg/flink/App1.java
@@ -4,43 +4,42 @@ import de.tum.in.msrg.datamodel.ClickEvent;
 import de.tum.in.msrg.datamodel.ClickUpdateEvent;
 import de.tum.in.msrg.datamodel.PageStatistics;
 import de.tum.in.msrg.datamodel.UpdateEvent;
-import de.tum.in.msrg.flink.connector.CustomKafkaSource;
 import de.tum.in.msrg.flink.functions.ClickUpdateJoinFunction;
 import de.tum.in.msrg.flink.functions.CountProcessWindowFunction;
-import de.tum.in.msrg.flink.serialization.ClickEventDeserializer;
-import de.tum.in.msrg.flink.serialization.PageStatisticsValueSerializer;
-import de.tum.in.msrg.flink.serialization.UpdateEventDeserializer;
+import de.tum.in.msrg.flink.serialization.*;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
-import org.apache.flink.connector.kafka.source.KafkaSourceOptions;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
-import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.flink.util.OutputTag;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.time.Duration;
 import java.util.Properties;
 
 /**
  * Hello world!
  *
  */
-public class App2
+public class App1
 {
-    private static final Logger LOGGER = LogManager.getLogger(App2.class);
+    private static final Logger LOGGER = LogManager.getLogger(App1.class);
 
     public static void main( String[] args ) throws Exception {
 
@@ -51,82 +50,91 @@ public class App2
         LOGGER.info(String.format("kafka: %s", kafka));
 //        String pg = cmdLine.getOptionValue("pg", "exactly once");
 
+
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
-        environment.disableOperatorChaining();
-
-
-        Properties srcProperties = new Properties();
-
-        srcProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka);
-        srcProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        srcProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "flink-topology");
 
 
         Properties sinkProperties = new Properties();
 
         sinkProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka);
-        sinkProperties.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 3000);
+        sinkProperties.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 30000);
 
 
-
-
-        CustomKafkaSource<ClickEvent> clickSource = CustomKafkaSource.<ClickEvent>builder()
-//        KafkaSource<ClickEvent> clickSource = KafkaSource.<ClickEvent>builder()
+        KafkaSource<ClickEvent> clickSource = KafkaSource.<ClickEvent>builder()
                 .setTopics("click")
                 .setBootstrapServers(kafka)
-                .setStartingOffsets(OffsetsInitializer.earliest())
-                .setProperties(srcProperties)
-                .setProperty(KafkaSourceOptions.CLIENT_ID_PREFIX.key(), "flink-topology")
+                .setGroupId("clickReader")
+                .setStartingOffsets(OffsetsInitializer.committedOffsets(OffsetResetStrategy.EARLIEST))
                 .setValueOnlyDeserializer(new ClickEventDeserializer())
                 .build();
 
-        CustomKafkaSource<UpdateEvent> updateSource = CustomKafkaSource.<UpdateEvent>builder()
-//        KafkaSource<UpdateEvent> updateSource = KafkaSource.<UpdateEvent>builder()
+        KafkaSource<UpdateEvent> updateSource = KafkaSource.<UpdateEvent>builder()
                 .setTopics("update")
                 .setBootstrapServers(kafka)
-                .setProperties(srcProperties)
-                .setProperty(KafkaSourceOptions.CLIENT_ID_PREFIX.key(), "flink-topology")
+                .setGroupId("updateReader")
+                .setStartingOffsets(OffsetsInitializer.committedOffsets(OffsetResetStrategy.EARLIEST))
                 .setValueOnlyDeserializer(new UpdateEventDeserializer())
                 .build();
+
 
         KafkaSink<PageStatistics> statsSink = KafkaSink.<PageStatistics>builder()
                 .setBootstrapServers(kafka)
                 .setKafkaProducerConfig(sinkProperties)
-                .setDeliverGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
-                .setTransactionalIdPrefix("kafka-sink").
+                .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                .setTransactionalIdPrefix("statSink").
                 setRecordSerializer(KafkaRecordSerializationSchema.builder()
                         .setTopic("output")
                         .setValueSerializationSchema(new PageStatisticsValueSerializer())
+                        .setKeySerializationSchema(new PageStatisticsKeyValueSerializer())
                         .build())
                 .build();
 
+        KafkaSink<ClickUpdateEvent> lateSink = KafkaSink.<ClickUpdateEvent>builder()
+                .setBootstrapServers(kafka)
+                .setKafkaProducerConfig(sinkProperties)
+                .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                .setTransactionalIdPrefix("lateSink").
+                setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setTopic("lateOutput")
+                        .setValueSerializationSchema(new ClickUpdateValueSerializer())
+                        .setKeySerializationSchema(new ClickUpdateKeySerializer())
+                        .build())
+                .build();
+
+
+
         WatermarkStrategy<ClickEvent> clickEventWatermarkStrategy = WatermarkStrategy
-                .<ClickEvent>forBoundedOutOfOrderness(Duration.ofMillis(200))
-                .withIdleness(Duration.ofMillis(200))
+                .<ClickEvent>forMonotonousTimestamps()
                 .withTimestampAssigner((element, recordTimestamp) -> element.getTimestamp().getTime());
 
         WatermarkStrategy<UpdateEvent> updateEventWatermarkStrategy = WatermarkStrategy
-                .<UpdateEvent>forBoundedOutOfOrderness(Duration.ofMillis(200))
-                .withIdleness(Duration.ofMillis(200))
+                .<UpdateEvent>forMonotonousTimestamps()
                 .withTimestampAssigner((element, recordTimestamp) -> element.getTimestamp().getTime());
 
 
 
-        environment.enableCheckpointing(3000, CheckpointingMode.AT_LEAST_ONCE);
         DataStreamSource<ClickEvent> clickSrc = environment.fromSource(clickSource, clickEventWatermarkStrategy, "ClickEvent-src");
         DataStreamSource<UpdateEvent> updateSrc = environment.fromSource(updateSource, updateEventWatermarkStrategy, "UpdateEvent-src");
+
+        OutputTag<ClickUpdateEvent> outputTag = new OutputTag<ClickUpdateEvent>("lateEvent", TypeInformation.of(ClickUpdateEvent.class));
 
         DataStream<ClickUpdateEvent> joinedStream = clickSrc.join(updateSrc)
                 .where(ClickEvent::getPage).equalTo(UpdateEvent::getPage)
                 .window(TumblingEventTimeWindows.of(Time.seconds(60)))
+//                .allowedLateness(Time.seconds(20))
                 .apply(new ClickUpdateJoinFunction());
 
-        DataStream<PageStatistics> statsStream = joinedStream
+
+
+        SingleOutputStreamOperator<PageStatistics> statsStream = joinedStream
                 .keyBy(ClickUpdateEvent::getPage)
                 .window(TumblingEventTimeWindows.of(Time.seconds(60)))
+//                .allowedLateness(Time.seconds(20))
+                .sideOutputLateData(outputTag)
                 .process(new CountProcessWindowFunction());
 
         statsStream.sinkTo(statsSink);
+        statsStream.getSideOutput(outputTag).sinkTo(lateSink);
 
         environment.execute();
 
