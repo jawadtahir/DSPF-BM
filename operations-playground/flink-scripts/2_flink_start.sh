298e6200461624a575226e06b11ab08e4bec633b
Jawad Tahir <ranajawadtahir@gmail.com>
2023-09-28 20:43:37 +0200

    Kinda working Flink scripts

--- operations-playground/flink-scripts/2_flink_start.sh
+++ operations-playground/flink-scripts/2_flink_start.sh
@@ -17,6 +17,10 @@ CREATE_TOPIC_SCRIPT="/home/foobar/Downloads/kafka_2.13-2.6.0/bin/kafka-topics.sh
 #done
 
 # Remove any previous utils containers
+echo "Removing any previous analysis containers..."
+docker stack rm analysis
+
+# Remove any previous utils containers
 echo "Removing any previous utils containers..."
 docker stack rm utils
 
@@ -44,20 +48,18 @@ echo "Sleeping for 20 seconds..."
 sleep 20s
 
 echo "Creating topics..."
-#${CREATE_TOPIC_SCRIPT} --topic click --create --partitions 3 --replication-factor 3 --bootstrap-server node6:9094
-#${CREATE_TOPIC_SCRIPT} --topic update --create --partitions 3 --replication-factor 3 --bootstrap-server node6:9094
-#${CREATE_TOPIC_SCRIPT} --topic output --create --partitions 3 --replication-factor 3 --bootstrap-server node6:9094
 
 ${CREATE_TOPIC_SCRIPT} --topic click --create  --bootstrap-server node1:9094 --replica-assignment 1001:1002:1003,1002:1001:1003,1003:1001:1002
 ${CREATE_TOPIC_SCRIPT} --topic update --create  --bootstrap-server node1:9094 --replica-assignment 1001:1002:1003,1002:1001:1003,1003:1001:1002
 ${CREATE_TOPIC_SCRIPT} --topic output --create  --bootstrap-server node1:9094 --replica-assignment 1001:1002:1003,1002:1001:1003,1003:1001:1002
+${CREATE_TOPIC_SCRIPT} --topic lateOutput --create  --bootstrap-server node1:9094 --replica-assignment 1001:1002:1003,1002:1001:1003,1003:1001:1002
 
 
 # Sleep for 10 seconds
 echo "Sleeping for 10 seconds..."
 sleep 10s
 
-# Start Storm containers
-echo "Starting Storm containers..."
+# Start Flink containers
+echo "Starting Flink containers..."
 docker stack deploy --prune -c docker-compose-flink.yaml flink
 
--- operations-playground/flink-scripts/3_experiment_start.sh
+++ operations-playground/flink-scripts/3_experiment_start.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+# Remove any previous analysis containers
+echo "Removing any previous analysis containers..."
+docker stack rm analysis
+
 
 # Remove any previous utils containers
 echo "Removing any previous utils containers..."
 docker stack rm utils
 
-export DELAY_COUNT=2
+export DELAY_COUNT=1000
 export DELAY_LENGTH=1
+export EVENTS_PER_WINDOW=5000
+export KAFKA_BOOTSTRAP=kafka1:9092
+export SHORT_DATA=""
 
 # Start utils containers
 echo "Starting utils containers..."
@@ -30,11 +37,18 @@ docker stack deploy --prune -c docker-compose-utils.yaml utils
 #echo "Removing datagen container..."
 #docker service rm utils_datagen
 
-echo "Sleeping for 480s (180+150+150)"
-sleep 480s
+echo "Sleeping for 50s (30+10+10)"
+sleep 50s
 
 echo "Removing datagen container..."
 docker service rm utils_datagen
+
+echo "Sleeping for 10s"
+sleep 10s
+
+echo "Running analysis scripts"
+docker stack deploy --prune -c docker-compose-analysis.yaml analysis
+
 
 
 
